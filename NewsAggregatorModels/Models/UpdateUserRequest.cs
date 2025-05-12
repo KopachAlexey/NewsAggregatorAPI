3e54adac5471763e8e448103e190d5a829eedea9
Алексей Копач <AlekseyKopach777@gmail.com>
2025-05-12 22:13:55 +0300

    Добавленна модель для обновления информации о пользователе UpdateUserRequest

---
+++ NewsAggregatorModels/Models/UpdateUserRequest.cs
@@ -0,0 +1,11 @@
+﻿
+namespace NewsAggregatorModels.Models
+{
+    public class UpdateUserRequest
+    {
+        public string? Login { get; set; }
+        public string? Email { get; set; }
+        public string Password { get; set; }
+        public string? NewPassword { get; set; }
+    }
+}
