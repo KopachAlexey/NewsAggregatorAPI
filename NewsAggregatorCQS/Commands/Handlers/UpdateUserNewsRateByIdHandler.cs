9e7ac1965abfb7d8815820e9cdad42f3b2b7ab27
Алексей Копач <AlekseyKopach777@gmail.com>
2025-05-12 22:03:39 +0300

    Добавленна комманда и её обработчик UpdateUserNewsRateById

---
+++ NewsAggregatorCQS/Commands/Handlers/UpdateUserNewsRateByIdHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NewsAggregatorData;
+
+namespace NewsAggregatorCQS.Commands.Handlers
+{
+    public class UpdateUserNewsRateByIdHandler : IRequestHandler<UpdateUserNewsRateByIdCommand>
+    {
+        readonly NewsAggregatorContext _dbContext;
+
+        public UpdateUserNewsRateByIdHandler(NewsAggregatorContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Handle(UpdateUserNewsRateByIdCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _dbContext.Users.SingleOrDefaultAsync(u => u.Id.Equals(request.UserId),
+                cancellationToken);
+            if (user is null)
+                return;
+            user.NewsMinRate = request.NewNewsMinRate;
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ NewsAggregatorCQS/Commands/UpdateUserNewsRateByIdCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace NewsAggregatorCQS.Commands
+{
+    public class UpdateUserNewsRateByIdCommand : IRequest
+    {
+        public Guid UserId { get; init; }
+        public double NewNewsMinRate {  get; init; }
+    }
+}
