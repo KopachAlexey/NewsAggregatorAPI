bbf5c7e36043260b2deca517528f550988e183c1
Алексей Копач <AlekseyKopach777@gmail.com>
2025-05-12 21:59:40 +0300

    Добавленно UpdateUserDTO

---
+++ NewsAggregatorCore/DTO/UpdateUserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace NewsAggregatorCore.DTO
+{
+    public class UpdateUserDTO
+    {
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string? NewLogin { get; set; }
+        public string? NewEmail { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
