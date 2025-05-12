d1b3500f86c2ffafb0cf308fb9f8111a8448da39
Алексей Копач <AlekseyKopach777@gmail.com>
2025-05-12 22:15:18 +0300

    Добавлен валидатор для проверки UpdateUserRequest

---
+++ NewsAggregatorModels/Validation/UpdateUserRequestValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using NewsAggregatorCore;
+using NewsAggregatorModels.Models;
+
+namespace NewsAggregatorModels.Validation
+{
+    public class UpdateUserRequestValidator : AbstractValidator<UpdateUserRequest>
+    {
+        public UpdateUserRequestValidator() 
+        {
+            RuleFor(u => u.Login)
+                .Length(NewsAggregatorConstants.MinLoginLenth, NewsAggregatorConstants.MaxLoginLenth)
+                .When(u => u.Login is not null);
+            RuleFor(u => u.Password)
+                .Length(NewsAggregatorConstants.MinPasswordLenth, NewsAggregatorConstants.MaxPasswordLenth);
+            RuleFor(u => u.NewPassword)
+                .Length(NewsAggregatorConstants.MinPasswordLenth, NewsAggregatorConstants.MaxPasswordLenth)
+                .When(u => u.NewPassword is not null);
+            RuleFor(u => u.Email)
+                .EmailAddress()
+                .When(u => u.Email is not null)
+                .MinimumLength(NewsAggregatorConstants.MinEmailLenth)
+                .When(u => u.Email is not null);
+        }
+    }
+}
