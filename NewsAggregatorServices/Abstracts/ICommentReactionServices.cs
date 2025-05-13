5c4db5120d0fa7d5036953f44507fcaea56e730e
Алексей Копач <AlekseyKopach777@gmail.com>
2025-05-13 18:01:49 +0300

    Исправленны названия методов для получения реакций к комментариям

--- NewsAggregatorServices/Abstracts/ICommentReactionServices.cs
+++ NewsAggregatorServices/Abstracts/ICommentReactionServices.cs
@@ -5,8 +5,8 @@ namespace NewsAggregatorServices.Abstracts
     public interface ICommentReactionServices
     {
         public Task<AddResourceResultDTO> AddReactionToCommentAsync(Guid commentId, Guid userId, string reactionName);
-        public Task<UserCommentReactionDTO[]> GetCommentReactionsAsync(Guid commentId, string reactionName);
+        public Task<UserCommentReactionDTO[]> GetCommentReactionsByNameAsync(Guid commentId, string reactionName);
         public Task<OperationResultDTO> UpdateReactionToCommentAsync(Guid commentId, Guid userId, string reactionName);
-        public Task<UserCommentReactionDTO?> GetCommentReactionAsync(Guid commentId, Guid userId);
+        public Task<UserCommentReactionDTO?> GetCommentReactionByUserIdAsync(Guid commentId, Guid userId);
     }
 }
